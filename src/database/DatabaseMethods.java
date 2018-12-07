693b2112fa11569b2765390a3b101daa85fb9462
Kartik Shankhavaram <shankhavaramk@gmail.com>
2018-12-07 13:26:32 +0530

    Added TODO

--- src/database/DatabaseMethods.java
+++ src/database/DatabaseMethods.java
@@ -1,5 +1,17 @@
 package database;
 
 public class DatabaseMethods {
-
+	
+	private DatabaseMethods() {
+		
+	}
+	
+	/* TODO: Make methods to interact with JDBC
+	 * public void execute(String query) throws Exception
+	 * public void get(String query) throws Exception
+	 * 
+	 * Make this class a singleton using a getInstance method
+	 */
+	
+	
 }
